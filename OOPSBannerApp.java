525ba5045b65ee7933d9e674e05f70022b12fccc
nishokeswara27-design <nishokeswara27@gmail.com>
2026-02-17 14:39:34 +0530

    oops banner

---
+++ OOPSBannerApp.java
@@ -0,0 +1,21 @@
+
+/**
+ * OOPSBannerApp
+ * 
+ * This program prints the text "OOPS" to the console.
+ * It demonstrates the basic structure of a Java application.
+ * 
+ * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+
+    /**
+     * Main method - Entry point of the Java application
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
